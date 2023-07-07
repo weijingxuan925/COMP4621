ad3664b7226185dc6d69617f1de45924854bbd0d
jingxuanWei <jn728702@dal.ca>
2023-07-07 22:22:22 +0800

    制作完成login表格

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+// 或者
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, // 或者 ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
